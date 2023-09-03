ec3f21765d107acb4aaf8c1812b02ddbeff4a489
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-09-03 02:02:12 -0600

    All fixed

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int count = 0;
+        boolean dividend2;
+        boolean dividend3;
+
+
+        while (count <= printToInclusive) {
+            dividend2 = count % 2 != 0;
+            dividend3 = count % 3 != 0;
+
+            if (count > 3 && dividend2 && dividend3) {
+                System.out.println(count);
+                ++count;
+            } else if (count == 2 || count == 3) {
+                System.out.println(count);
+                ++count;
+            } else {
+                ++count;
+            }
+        }
     }
 }
